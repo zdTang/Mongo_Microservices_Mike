5e045957a6db5a50ae1a3b9625e2595a18fb46c3
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-10-11 10:05:50 -0400

    2- Seed database and migration

--- Mongo.Services.CouponAPI/Program.cs
+++ Mongo.Services.CouponAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Mongo.Services.CouponAPI.Data;
+
 namespace Mongo.Services.CouponAPI
 {
     public class Program
@@ -7,9 +10,12 @@ namespace Mongo.Services.CouponAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
+			// Add services to the container.
+			builder.Services.AddDbContext<AppDbContext>(option =>
+			{
+				option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+			});
+			builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -30,7 +36,28 @@ namespace Mongo.Services.CouponAPI
 
             app.MapControllers();
 
-            app.Run();
-        }
-    }
+			ApplyMigration();
+
+			app.Run();
+
+
+
+
+			void ApplyMigration()
+			{
+				using (var scope = app.Services.CreateScope())
+				{
+					var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+					if (_db.Database.GetPendingMigrations().Count() > 0)
+					{
+						_db.Database.Migrate();
+					}
+				}
+			}
+		}
+
+
+		
+	}
 }
