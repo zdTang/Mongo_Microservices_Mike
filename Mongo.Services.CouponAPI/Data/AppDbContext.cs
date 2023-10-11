1d6ff8b2687a3a68c5717cfff3dcfd5a52fe2530
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-10-11 09:24:53 -0400

    2- Create dbContext

---
+++ Mongo.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mongo.Services.CouponAPI.Models;
+
+namespace Mongo.Services.CouponAPI.Data
+{
+	public class AppDbContext:DbContext
+	{
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+                
+        }
+		public DbSet<Coupon> Coupons { get; set; }
+	}
+}
--- Mongo.Services.CouponAPI/Models/Coupon.cs
+++ Mongo.Services.CouponAPI/Models/Coupon.cs
@@ -1,9 +1,14 @@
-﻿namespace Mongo.Services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mongo.Services.CouponAPI.Models
 {
 	public class Coupon
 	{
+		[Key]
 		public int CouponId { get; set; }
+		[Required]
 		public string CouponCode { get; set; }
+		[Required]
 		public double DiscountAmount { get; set; }
 		public int MinAmount { get; set; }
 	}
