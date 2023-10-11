c1edb6d55cf5c2d0fd67b873857151bcde513446
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-10-11 10:16:45 -0400

    2-Get all coupons or get coupon by ID

---
+++ Mongo.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mongo.Services.CouponAPI.Data;
+using Mongo.Services.CouponAPI.Models;
+
+namespace Mongo.Services.CouponAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CouponAPIController : ControllerBase
+	{
+		private readonly AppDbContext _db;
+
+		public CouponAPIController(AppDbContext db)
+		{
+			_db = db;
+		}
+
+		[HttpGet]
+		public object Get()
+		{
+			try
+			{
+				IEnumerable<Coupon> objList = _db.Coupons.ToList();
+				return objList;
+			}
+			catch (Exception ex)
+			{
+
+			}
+			return null;
+		}
+
+		[HttpGet]
+		[Route("{id:int}")]
+		public object Get(int id)
+		{
+			try
+			{
+				Coupon objList = _db.Coupons.First(u => u.CouponId == id);
+				return objList;
+			}
+			catch (Exception ex)
+			{
+
+			}
+			return null;
+		}
+	}
+}
