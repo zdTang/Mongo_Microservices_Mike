09ac8f542e28b2ebff5bf1534cc00c1062b65f04
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-10-17 11:33:45 -0400

    3- Create and Register Services in Program Class File

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,3 +1,7 @@
+using Mango.Web.Service.IService;
+using Mango.Web.Service;
+using Mango.Web.Utility;
+
 namespace Mango.Web
 {
     public class Program
@@ -8,6 +12,13 @@ namespace Mango.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddHttpClient();
+            builder.Services.AddHttpClient<ICouponService, CouponService>();
+            SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
+
+            builder.Services.AddScoped<IBaseService, BaseService>();
+            builder.Services.AddScoped<ICouponService, CouponService>();
 
             var app = builder.Build();
 
